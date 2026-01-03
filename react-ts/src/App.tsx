fd176cf5024e6283cefd1ca8a4446d9020ab0243
basejumpa <basejumpa@encouraged-coders.de>
2026-01-03 16:13:08 +0100

    Make use of Leva: Select color

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -2,16 +2,30 @@ import './App.css';
 import type { FC } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Grid } from '@react-three/drei';
+import { Leva, useControls } from 'leva';
 
-const App: FC = () => (
-    <Canvas >
-        <mesh>
-            <boxGeometry />
-            <meshBasicMaterial color="orange" />
-        </mesh>
-        <Grid />
-        <OrbitControls />
-    </Canvas >
-);
+const App: FC = () => {
+    const { color } = useControls({
+        color: {
+            label: 'Color',
+            value: 'orange',
+            options: ['orange', 'green', 'blue'],
+        },
+    });
+
+    return (
+        <>
+            <Canvas>
+                <mesh>
+                    <boxGeometry />
+                    <meshBasicMaterial color={color} />
+                </mesh>
+                <Grid />
+                <OrbitControls />
+            </Canvas>
+            <Leva collapsed />
+        </>
+    );
+};
 
 export default App;
