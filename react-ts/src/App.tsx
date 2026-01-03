9c60bf502cf4d5c07862163dd470f1c56c18360b
basejumpa <basejumpa@encouraged-coders.de>
2026-01-03 16:31:01 +0100

    Add camera controller

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,11 +1,27 @@
 import './App.css';
 import type { FC } from 'react';
-import { Canvas } from '@react-three/fiber';
+import { useEffect } from 'react';
+import { Canvas, useThree } from '@react-three/fiber';
 import { OrbitControls, Grid } from '@react-three/drei';
 import { Leva, useControls } from 'leva';
 
+type CameraControllerProps = {
+    cameraZ: number;
+};
+
+const CameraController: FC<CameraControllerProps> = ({ cameraZ }) => {
+    const { camera } = useThree();
+
+    useEffect(() => {
+        camera.position.set(camera.position.x, camera.position.y, cameraZ);
+        camera.updateProjectionMatrix();
+    }, [camera, cameraZ]);
+
+    return null;
+};
+
 const App: FC = () => {
-    const { color, wireframe } = useControls({
+    const { color, wireframe, cameraZ } = useControls({
         color: {
             label: 'Color',
             value: 'orange',
@@ -13,13 +29,21 @@ const App: FC = () => {
         },
         wireframe: {
             label: 'Wireframe',
-            value: false,
+            value: true,
+        },
+        cameraZ: {
+            label: 'Camera Z',
+            value: 8,
+            min: 2,
+            max: 20,
+            step: 1,
         },
     });
 
     return (
         <>
-            <Canvas>
+            <Canvas camera={{ position: [0, 0, cameraZ], fov: 50 }}>
+                <CameraController cameraZ={cameraZ} />
                 <mesh>
                     <boxGeometry />
                     <meshBasicMaterial color={color} wireframe={wireframe} />
@@ -27,7 +51,7 @@ const App: FC = () => {
                 <Grid />
                 <OrbitControls />
             </Canvas>
-            <Leva collapsed />
+            <Leva />
         </>
     );
 };
