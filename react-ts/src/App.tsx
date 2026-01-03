690b353cb19645d7392869b581de5629764f23b1
basejumpa <basejumpa@encouraged-coders.de>
2026-01-03 16:32:45 +0100

    Remove camera controller

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -5,23 +5,9 @@ import { Canvas, useThree } from '@react-three/fiber';
 import { OrbitControls, Grid } from '@react-three/drei';
 import { Leva, useControls } from 'leva';
 
-type CameraControllerProps = {
-    cameraZ: number;
-};
-
-const CameraController: FC<CameraControllerProps> = ({ cameraZ }) => {
-    const { camera } = useThree();
-
-    useEffect(() => {
-        camera.position.set(camera.position.x, camera.position.y, cameraZ);
-        camera.updateProjectionMatrix();
-    }, [camera, cameraZ]);
-
-    return null;
-};
 
 const App: FC = () => {
-    const { color, wireframe, cameraZ } = useControls({
+    const { color, wireframe } = useControls({
         color: {
             label: 'Color',
             value: 'orange',
@@ -31,19 +17,11 @@ const App: FC = () => {
             label: 'Wireframe',
             value: true,
         },
-        cameraZ: {
-            label: 'Camera Z',
-            value: 8,
-            min: 2,
-            max: 20,
-            step: 1,
-        },
     });
 
     return (
         <>
-            <Canvas camera={{ position: [0, 0, cameraZ], fov: 50 }}>
-                <CameraController cameraZ={cameraZ} />
+            <Canvas>
                 <mesh>
                     <boxGeometry />
                     <meshBasicMaterial color={color} wireframe={wireframe} />
